abc780e917184957520477b4b05227cf4b411f71
John Pretz <pretz@icecube.umd.edu>
2004-03-10 20:31:51 +0000

    don't recall the change in this.
    git-svn-id: http://code.icecube.wisc.edu/svn/phys-services/trunk@4449 16731396-06f5-0310-8873-f7f720988828

--- project.mk
+++ project.mk
@@ -1,6 +1,6 @@
 ROOT_BIN_NAMES := 
 ROOT_EXCLUDE_NAMES := 
-ROOT_NON_CLASSES := 
+ROOT_NON_CLASSES := PhotonicsMath PhotonicsExternal 
 ROOT_TEMPLATES := 
 USES_PROJECTS := dataclasses icetray 
 USES_TOOLS := root PSInterface
