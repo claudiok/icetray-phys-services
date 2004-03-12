af6034b42ab3c55f72022b7153c2fedc54182419
John Pretz <pretz@icecube.umd.edu>
2004-03-12 14:48:08 +0000

    Removing the dependency to PSInterface.  Trying to make it friendly for someone who doesn't want to hack with bfd.
    git-svn-id: http://code.icecube.wisc.edu/svn/phys-services/trunk@4450 16731396-06f5-0310-8873-f7f720988828

--- project.mk
+++ project.mk
@@ -1,6 +1,6 @@
 ROOT_BIN_NAMES := 
 ROOT_EXCLUDE_NAMES := 
-ROOT_NON_CLASSES := PhotonicsMath PhotonicsExternal 
+ROOT_NON_CLASSES :=  
 ROOT_TEMPLATES := 
 USES_PROJECTS := dataclasses icetray 
-USES_TOOLS := root PSInterface
+USES_TOOLS := root #PSInterface
