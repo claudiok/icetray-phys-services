cfa6a9b9aff255d2b118601d76801caec454af9d
John Pretz <pretz@icecube.umd.edu>
2004-07-21 15:51:45 +0000

    added in helper methods for accessing the event and the geometry from the frame
    git-svn-id: http://code.icecube.wisc.edu/svn/phys-services/trunk@4471 16731396-06f5-0310-8873-f7f720988828

--- public/phys-services/I3PhysicsModule.h
+++ public/phys-services/I3PhysicsModule.h
@@ -1,11 +1,11 @@
 /**
  * copyright  (C) 2004
  * the icecube collaboration
- * $Id: I3PhysicsModule.h,v 1.1 2004/07/16 19:41:05 pretz Exp $
+ * $Id: I3PhysicsModule.h,v 1.2 2004/07/21 15:51:45 pretz Exp $
  *
  * @file I3PhysicsModule.h
- * @version $Revision: 1.1 $
- * @date $Date: 2004/07/16 19:41:05 $
+ * @version $Revision: 1.2 $
+ * @date $Date: 2004/07/21 15:51:45 $
  * @author pretz
  */
 
@@ -14,18 +14,27 @@
 
 #include "icetray/I3Module.h"
 #include "icetray/I3ContextAccess.h"
+#include "dataclasses/I3Event.h"
+#include "dataclasses/I3Geometry.h"
+#include "icetray/I3FrameAccess.h"
 #include "I3ParticleDataService.h"
 #include "I3Calculator.h"
 #include "I3RandomService.h"
 
+using namespace std;
+
 /**
- * A class which provides helper methods to access standard services.
+ * @brief A class which provides helper methods to avoid cumbersome syntax.
+ * 
  * If a module inherits from this class, it doesn't have to go through
- * the cumbersome I3ContextAccess<>::GetService() syntax
+ * the cumbersome I3ContextAccess<>::GetService() syntax.  
+ * Can also avoid the I3FrameAccess syntax using the GetEvent and GetGeometry
+ * methods
  *
  * @author pretz
  * @todo provide default 'names' for each service to match the default names
  * the services are created with
+ * @todo add methods to access other stream objects when they are defined
  */
 class I3PhysicsModule : public I3Module
 {
@@ -75,6 +84,31 @@ class I3PhysicsModule : public I3Module
       return I3ContextAccess<I3RandomService>::
 	GetService(GetContext(),name);
     }
+
+  /**
+   * Gets an event out of a frame.  Just a helper method to simplify the
+   * syntax.
+   * @return an I3Event that is in the frame
+   * @param frame the frame that you want the event out of.
+   * @param name the name of the event in the frame, defaults to "Physics"
+   */
+  I3Event& GetEvent(I3Frame& frame,const string& name = "Physics")
+    {
+      return I3FrameAccess<I3Event>::Get(frame,name);
+    }
+
+  /**
+   * Gets the geometry out of the frame.  Just a helper method to simplify the
+   * syntax
+   * @return an I3Geometry that is in the frame
+   * @param frame the frame that you want the event out of
+   */
+  I3Geometry& GetGeometry(I3Frame& frame,const string& name="Geometry")
+    {
+      return I3FrameAccess<I3Geometry>::Get(frame,name);
+    }
+
+
 
  private:
 
