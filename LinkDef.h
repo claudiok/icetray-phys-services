37698f52b4d64f38cbf6a41185734d0682286d1d
Troy Straszheim <troy@icecube.umd.edu>
2005-01-22 16:11:13 +0000

    explicit linkdef
    git-svn-id: http://code.icecube.wisc.edu/svn/phys-services/trunk@4535 16731396-06f5-0310-8873-f7f720988828

---
+++ LinkDef.h
@@ -0,0 +1,19 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class I3Calculator;
+#pragma link C++ class I3CalculatorFactoryImpl;
+#pragma link C++ class I3CalculatorImpl;
+#pragma link C++ class I3GSLRandomService;
+#pragma link C++ class I3GSLRandomServiceFactory;
+#pragma link C++ class I3ParticleDataService;
+#pragma link C++ class I3ParticleDataServiceFactory;
+#pragma link C++ class I3PhysicsModule;
+#pragma link C++ class I3RandomService;
+#pragma link C++ class I3TRandomService;
+#pragma link C++ class I3TRandomServiceFactory;
+
+#endif
