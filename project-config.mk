120891355ea1835edb1f7a755a01341e10885e12
Troy Straszheim <troy@icecube.umd.edu>
2005-02-24 14:55:23 +0000

    ROOT DETANGLEMENT: now you specify what headers you want rootcint to see, and what you want dictionaries for in the linkdef.   See the offline-mk docs and the emails on icerec/ice3sim
    git-svn-id: http://code.icecube.wisc.edu/svn/phys-services/trunk@4543 16731396-06f5-0310-8873-f7f720988828

--- project-config.mk
+++ project-config.mk
@@ -1,15 +1,7 @@
-
 TEST_SRCDIR_NAMES := test
 
 # Anything you don't want to compile (must be full path?)
 CXX_EXCLUDE_SRCS :=
-
-# ROOT_EXCLUDE_NAMES :=  what was this?
-
-# has filtered out from what is automagically put into linkdef
-ROOT_NON_CLASSES :=  
-# exactly same function as ROOT_NON_CLASSES
-ROOT_TEMPLATES := 
 
 # public directories for these projects are added to #include path,
 # and libraries are added to link.
@@ -18,5 +10,10 @@ USES_PROJECTS := dataclasses icetray
 # same as above, but for external tools.  Paths found in config.mk
 USES_TOOLS := root roost log4cplus TUT gsl boost
 
-# this is now globally "root" by default
-BUILD_FLAVOR := root
+ROOT_DICTIONARY_HEADERS := \
+phys-services/I3Calculator.h		   phys-services/I3ParticleDataServiceFactory.h\
+phys-services/I3CalculatorFactoryImpl.h    phys-services/I3PhysicsModule.h\
+phys-services/I3CalculatorImpl.h	   phys-services/I3RandomService.h\
+phys-services/I3GSLRandomService.h	   phys-services/I3TRandomService.h\
+phys-services/I3GSLRandomServiceFactory.h  phys-services/I3TRandomServiceFactory.h\
+phys-services/I3ParticleDataService.h
