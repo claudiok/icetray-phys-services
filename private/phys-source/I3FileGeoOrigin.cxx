5b5832f6aff331fba880b23dc3830ceca14003c9
Ralf Ehrlich <ehrlich@icecube.umd.edu>
2005-03-17 21:26:11 +0000

    added the IceTop part
    git-svn-id: http://code.icecube.wisc.edu/svn/phys-services/trunk@4561 16731396-06f5-0310-8873-f7f720988828

--- private/phys-source/I3FileGeoOrigin.cxx
+++ private/phys-source/I3FileGeoOrigin.cxx
@@ -1,6 +1,8 @@
 #include "phys-source/I3FileGeoOrigin.h"
 #include "dataclasses/I3OMGeoAMANDA.h"
 #include "dataclasses/I3OMGeoIceCube.h"
+#include "dataclasses/I3StationMap.h"
+#include "dataclasses/I3SurfModuleGeo.h"
 #include "icetray/I3TrayHeaders.h"
 
 void I3FileGeoOrigin::Fatal(const string& message)
@@ -79,15 +81,44 @@ void I3FileGeoOrigin::FillGeometryFromFile(I3Geometry& Geometry,
   
   while(I3GeoInFile>>string_F>>tube_F>>x_F>>y_F>>z_F>>orientation_F)
     {
-      I3OMGeoIceCubePtr icecube = I3OMGeoIceCubePtr(new I3OMGeoIceCube());
-      Geometry.GetInIceGeometry()[OMKey(string_F,tube_F)] = icecube;
+      if(tube_F<61)
+      {
+        I3OMGeoIceCubePtr icecube = I3OMGeoIceCubePtr(new I3OMGeoIceCube());
+        Geometry.GetInIceGeometry()[OMKey(string_F,tube_F)] = icecube;
       
-      icecube->SetPos(x_F * I3Units::m,
-		      y_F * I3Units::m,
-		      z_F * I3Units::m);
-      icecube->SetOrientation(I3OMGeo::Down); 
-      icecube->SetArea(0.0444 * I3Units::m2);
-      icecube->SetRelativeQE(1.0);
+        icecube->SetPos(x_F * I3Units::m,
+                        y_F * I3Units::m,
+		        z_F * I3Units::m);
+        icecube->SetOrientation(I3OMGeo::Down); 
+        icecube->SetArea(0.0444 * I3Units::m2);
+        icecube->SetRelativeQE(1.0);
+      }
+      else
+      {
+        I3StationMap &station_map = Geometry.GetIceTopGeometry().GetStationMap();
+        if(station_map.find(string_F)==station_map.end()) 
+        {
+          station_map[string_F] = I3StationGeoPtr(new I3StationGeo);
+          station_map[string_F]->push_back(I3SurfModuleGeoPtr(new I3SurfModuleGeo()));
+          station_map[string_F]->push_back(I3SurfModuleGeoPtr(new I3SurfModuleGeo()));
+        }
+
+        I3OMGeoIceCubePtr icecube = I3OMGeoIceCubePtr(new I3OMGeoIceCube());
+        switch(tube_F)
+        {
+          case 61:
+          case 62: (*(*station_map[string_F])[0])[OMKey(string_F, tube_F)] = icecube; break;
+          case 63:
+          case 64: (*(*station_map[string_F])[1])[OMKey(string_F, tube_F)] = icecube; break;
+          default: Fatal("Got a wrong tube number.");
+        }
+        icecube->SetPos(x_F * I3Units::m,
+                        y_F * I3Units::m,
+		        z_F * I3Units::m);
+        icecube->SetOrientation(I3OMGeo::Down); 
+        icecube->SetArea(0.0444 * I3Units::m2);
+        icecube->SetRelativeQE(1.0);
+      }
     }
   
   AmaGeoInFile.close();
