4493b65facc0e03dd05d59f41a127eab692ef0aa
Ralf Ehrlich <ehrlich@icecube.umd.edu>
2005-06-15 22:30:18 +0000

    added I3DummyCalibrationSource
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@9186 16731396-06f5-0310-8873-f7f720988828

--- LinkDef.h
+++ LinkDef.h
@@ -20,6 +20,7 @@
 #pragma link C++ class I3DummyMonitoringOriginFactory;
 #pragma link C++ class I3DummyGeoOriginFactory;
 #pragma link C++ class I3DummyCalibOriginFactory;
+#pragma link C++ class I3DummyCalibrationSource;
 #pragma link C++ class I3DummyDetectorStatusOriginFactory;
 #pragma link C++ class I3DummyDetectorStatusSource;
 #pragma link C++ class I3FileGeoOriginFactory;
