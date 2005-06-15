e890fd3c73befc132c126f8c9bba7ddd3f1c0f8e
John Pretz <pretz@icecube.umd.edu>
2005-06-15 16:15:19 +0000

    tweak to ENSURE statements
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@9176 16731396-06f5-0310-8873-f7f720988828

--- private/test/SimpleOrderingTest-Monitoring.h
+++ private/test/SimpleOrderingTest-Monitoring.h
@@ -36,12 +36,11 @@ inline void Client::CheckFrame<0>(I3Frame& frame)
 {
   log_debug("FrameStream: %s",DumpStop(frame).c_str());
   ENSURE(DumpStop(frame) == "Geometry");
-  ENSURE("geometry frame has geometry",
-	      I3FrameAccess<I3Geometry>::Exists(frame,
-						"Geometry"));
-  ENSURE("geometry frame has geometry header",
-	      I3FrameAccess<I3GeometryHeader>::Exists(frame,
-						      "GeometryHeader"));
+  ENSURE(I3FrameAccess<I3Geometry>::Exists(frame,
+					   "Geometry"));
+  ENSURE(I3FrameAccess<I3GeometryHeader>::Exists(frame,
+						 "GeometryHeader"));
+
   cout<<I3FrameAccess<I3GeometryHeader>::Get(frame,"GeometryHeader")<<endl;
   cout<<I3FrameAccess<I3GeometryHeader>::Get(frame,"Geometry")<<endl;
 }
@@ -50,18 +49,14 @@ inline void Client::CheckFrame<1>(I3Frame& frame)
 {
   log_debug("FrameStream: %s",DumpStop(frame).c_str());
   ENSURE(DumpStop(frame) == "Calibration");
-  ENSURE("calibration frame has calibration",
-	      I3FrameAccess<I3Calibration>::Exists(frame,"Calibration"));
-  ENSURE("calibration frame has calibration header",
-	      I3FrameAccess<I3CalibrationHeader>::
-	      Exists(frame,
-		     "CalibrationHeader"));
-  ENSURE("calibration frame has geometry",
-	      I3FrameAccess<I3Geometry>::Exists(frame,
+  ENSURE(I3FrameAccess<I3Calibration>::Exists(frame,"Calibration"));
+  ENSURE(I3FrameAccess<I3CalibrationHeader>::
+	 Exists(frame,
+		"CalibrationHeader"));
+  ENSURE(I3FrameAccess<I3Geometry>::Exists(frame,
 						"Geometry"));
-  ENSURE("calibration frame has geometry header",
-	      I3FrameAccess<I3GeometryHeader>::Exists(frame,
-						      "GeometryHeader"));
+  ENSURE(I3FrameAccess<I3GeometryHeader>::Exists(frame,
+						 "GeometryHeader"));
   cout<<I3FrameAccess<I3CalibrationHeader>::Get(frame,
 						"CalibrationHeader")
       <<endl;
@@ -74,22 +69,16 @@ inline void Client::CheckFrame<2>(I3Frame& frame)
 {
   log_debug("FrameStream: %s",DumpStop(frame).c_str());
   ENSURE(DumpStop(frame) == "DetectorStatus");
-  ENSURE("detector status frame has geometry",
-	      I3FrameAccess<I3Geometry>::Exists(frame,
-						"Geometry"));
-  ENSURE("detector status frame has geometry header",
-	      I3FrameAccess<I3GeometryHeader>::Exists(frame,
-						      "GeometryHeader"));
-  ENSURE("detector status frame has detector status",
-	      I3FrameAccess<I3DetectorStatus>::
-	      Exists(frame,"DetectorStatus"));
-  ENSURE("detector status frame has detector status header",
-	      I3FrameAccess<I3DetectorStatusHeader>::
-	      Exists(frame,"DetectorStatus"));
-  ENSURE("detector status frame has calibration",
-	      I3FrameAccess<I3Calibration>::Exists(frame,"Calibration"));
-  ENSURE("detector status frame has calibration header",
-	      I3FrameAccess<I3CalibrationHeader>::
+  ENSURE(I3FrameAccess<I3Geometry>::Exists(frame,
+					   "Geometry"));
+  ENSURE(I3FrameAccess<I3GeometryHeader>::Exists(frame,
+						 "GeometryHeader"));
+  ENSURE(I3FrameAccess<I3DetectorStatus>::
+	 Exists(frame,"DetectorStatus"));
+  ENSURE(I3FrameAccess<I3DetectorStatusHeader>::
+	 Exists(frame,"DetectorStatus"));
+  ENSURE(I3FrameAccess<I3Calibration>::Exists(frame,"Calibration"));
+  ENSURE(I3FrameAccess<I3CalibrationHeader>::
 	      Exists(frame,
 		     "CalibrationHeader"));
   
@@ -108,22 +97,16 @@ inline void Client::CheckFrame<3>(I3Frame& frame)
 {
   log_debug("FrameStream: %s",DumpStop(frame).c_str());
   ENSURE(DumpStop(frame) == "Monitor");
-  ENSURE("event frame has geometry",
-	      I3FrameAccess<I3Geometry>::Exists(frame,"Geometry"));
-  ENSURE("event frame has geometry header",
-	      I3FrameAccess<I3GeometryHeader>::
+  ENSURE(I3FrameAccess<I3Geometry>::Exists(frame,"Geometry"));
+  ENSURE(I3FrameAccess<I3GeometryHeader>::
 	      Exists(frame,"GeometryHeader"));
-  ENSURE("event frame has calibration",
-	      I3FrameAccess<I3Calibration>::Exists(frame,
+  ENSURE(I3FrameAccess<I3Calibration>::Exists(frame,
 						   "Calibration"));
-  ENSURE("event frame has calibration header",
-	      I3FrameAccess<I3CalibrationHeader>
+  ENSURE(I3FrameAccess<I3CalibrationHeader>
 	      ::Exists(frame,
 		       "CalibrationHeader"));
-  ENSURE("event frame has event",
-		I3FrameAccess<I3Monitoring>::Exists(frame,"Monitor"));
-  ENSURE("event frame has event header",
-	      I3FrameAccess<I3MonitoringHeader>::Exists(frame,"MonitorHeader"));
+  ENSURE(I3FrameAccess<I3Monitoring>::Exists(frame,"Monitor"));
+  ENSURE(I3FrameAccess<I3MonitoringHeader>::Exists(frame,"MonitorHeader"));
   cout<<I3FrameAccess<I3MonitoringHeader>::Get(frame,"MonitorHeader")<<endl;
   cout<<I3FrameAccess<I3Monitoring>::Get(frame,"Monitor")<<endl;
 
@@ -133,27 +116,19 @@ inline void Client::CheckFrame<4>(I3Frame& frame)
 {
   log_debug("FrameStream: %s",DumpStop(frame).c_str());
   ENSURE(DumpStop(frame) == "Monitor");
-  ENSURE("event frame has geometry",
-	      I3FrameAccess<I3Geometry>::Exists(frame,"Geometry"));
-  ENSURE("event frame has geometry header",
-	      I3FrameAccess<I3GeometryHeader>::
+  ENSURE(I3FrameAccess<I3Geometry>::Exists(frame,"Geometry"));
+  ENSURE(I3FrameAccess<I3GeometryHeader>::
 		Exists(frame,"GeometryHeader"));
-  ENSURE("event frame has calibration",
-	      I3FrameAccess<I3Calibration>::Exists(frame,"Calibration"));
-  ENSURE("event frame has calibration header",
-	      I3FrameAccess<I3CalibrationHeader>::
+  ENSURE(I3FrameAccess<I3Calibration>::Exists(frame,"Calibration"));
+  ENSURE(I3FrameAccess<I3CalibrationHeader>::
 	      Exists(frame,
 		     "CalibrationHeader"));
-  ENSURE("event frame has detector status",
-	      I3FrameAccess<I3DetectorStatus>::
+  ENSURE(I3FrameAccess<I3DetectorStatus>::
 	      Exists(frame,"DetectorStatus"));
-  ENSURE("event frame has detector status header",
-	      I3FrameAccess<I3DetectorStatusHeader>::
+  ENSURE(I3FrameAccess<I3DetectorStatusHeader>::
 	      Exists(frame,"DetectorStatus"));
-  ENSURE("event frame has event",
-	      I3FrameAccess<I3Monitoring>::Exists(frame,"Monitor"));
-  ENSURE("event frame has event header",
-	      I3FrameAccess<I3MonitoringHeader>::Exists(frame,"MonitorHeader"));
+  ENSURE(I3FrameAccess<I3Monitoring>::Exists(frame,"Monitor"));
+  ENSURE(I3FrameAccess<I3MonitoringHeader>::Exists(frame,"MonitorHeader"));
   cout<<I3FrameAccess<I3MonitoringHeader>::Get(frame,"MonitorHeader")<<endl;
   cout<<I3FrameAccess<I3Monitoring>::Get(frame,"Monitor")<<endl;
 }
