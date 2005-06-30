2cb4d4932af8c565730bd3b902885a3e8c6fce7f
Tom McCauley <tpmccauley@lbl.gov>
2005-06-30 19:16:48 +0000

    Ooops forgot to update this
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@9642 16731396-06f5-0310-8873-f7f720988828

--- resources/scripts/TestSource.py
+++ resources/scripts/TestSource.py
@@ -18,9 +18,11 @@ load('libDOMcalibrator.so')
 tray = I3Tray()
 
 F2kCascadeFile = expandvars('$I3_WORK/hit-constructor/resources/gevshower.f2k')
+channel_id_file = expandvars('$I3_WORK/amanda-core/resources/default_geometry.f2k');
 
 tray.AddService('I3F2kFileFactory', 'f2k')
 tray.SetParameter('f2k', 'Infile', F2kCascadeFile)
+tray.SetParameter('f2k', 'ChannelID_file', channel_id_file);
 
 tray.AddModule('I3F2kEventSource', 'f2kevent')
 tray.SetParameter('f2kevent', 'NumberEvents', 2)
@@ -52,8 +54,6 @@ tray.SetParameter('domstatusfiller', 'speThreshold', 4.0)
 
 tray.AddModule('I3CalibrateStatusModule', 'calibratestatus')
 
-tray.AddModule('I3TestSources', 'testsources')
-
 tray.AddModule('I3HitConstructor', 'hits')
 
 tray.AddModule('I3RomeoInterfaceModule', 'romeo')
@@ -81,8 +81,7 @@ tray.ConnectBoxes('calibration', 'OutBox', 'geometry')
 tray.ConnectBoxes('geometry', 'OutBox', 'calibfiller')
 tray.ConnectBoxes('calibfiller', 'OutBox', 'domstatusfiller')
 tray.ConnectBoxes('domstatusfiller', 'OutBox', 'calibratestatus')
-tray.ConnectBoxes('calibratestatus', 'OutBox', 'testsources')
-tray.ConnectBoxes('testsources', 'OutBox', 'hits')
+tray.ConnectBoxes('calibratestatus', 'OutBox', 'hits')
 tray.ConnectBoxes('hits', 'OutBox', 'romeo')
 tray.ConnectBoxes('romeo', 'OutBox', 'domsimulator')
 tray.ConnectBoxes('domsimulator', 'OutBox', 'domcalibrator')
