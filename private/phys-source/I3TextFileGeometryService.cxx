fc5777615e5b1b171adacb7f91514405b03f2e3c
John Pretz <pretz@icecube.umd.edu>
2006-01-31 02:32:36 +0000

    text file geometry service re-re-re addded
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/branches/dc2@15054 16731396-06f5-0310-8873-f7f720988828

---
+++ private/phys-source/I3TextFileGeometryService.cxx
@@ -0,0 +1,131 @@
+#include "phys-services/source/I3TextFileGeometryService.h"
+#include "dataclasses/geometry/I3OMGeo.h"
+#include "dataclasses/geometry/I3StationMap.h"
+#include "dataclasses/geometry/I3SurfModuleGeo.h"
+#include "icetray/I3TrayHeaders.h"
+
+void I3TextFileGeometryService::Fatal(const string& message)
+{
+  log_fatal(message.c_str());
+}
+
+I3GeometryPtr I3TextFileGeometryService::GetGeometry(I3Time time)
+{
+  I3GeometryPtr geometry = I3GeometryPtr(new I3Geometry());
+  FillGeometryFromFile(*geometry);
+  I3Time start;
+  start.SetDaqTime(0,0);
+  I3Time end;
+  end.SetDaqTime(3000,0);
+  geometry->SetStartTime(start);
+  geometry->SetEndTime(end);
+  return geometry;
+}
+
+void I3TextFileGeometryService::FillGeometryFromFile(I3Geometry& Geometry)
+{
+  //
+  // NB: This code was cut and pasted with minor tweaks from the I3Db code.
+  // The author is Georges Kohnen
+  //
+  // Any bugs are probably my fault, and any praises should
+  // probably go to him. -JPretz
+  //
+  
+  //Create GeoInFile
+  ifstream AmaGeoInFile;	
+  ifstream I3GeoInFile;		
+  log_warn("I3Db: Reading IceCube Geometry from file");
+  
+  //Open the files ama.geo and icecube.geo
+  AmaGeoInFile.open(fAmaGeoInputFile.c_str(), ifstream::in); 	
+  I3GeoInFile.open(fI3GeoInputFile.c_str(), ifstream::in); 
+  
+  //Did the files open correctly?
+  if (AmaGeoInFile.fail())
+    Fatal("I3Db: The specified AMANDA geometry file does not exist "
+	  "or did not want to be opened!");
+  if (I3GeoInFile.fail())
+    Fatal("I3Db: The specified IceCube geometry file does not exist "
+	  "or did not want to be opened!");	
+  
+  log_info("I3Db: FILES opened successfully. Getting the geometry "
+	   "and filling it into the frame");
+  log_info("Coordinates of OM's in meters \n\n");
+  
+  //The variables declared hereafter are followed by "_F" to denote 
+  //that they serve the
+  //"Fill Geometry From File" method and not the DB Access method...
+  
+  int string_F;
+  int tube_F;
+  int orientation_F;		
+  double x_F, y_F, z_F;
+  
+  while(AmaGeoInFile>>string_F>>tube_F>>x_F>>y_F>>z_F>>orientation_F)
+    {    
+      I3OMGeoPtr amanda = I3OMGeoPtr(new I3OMGeo());
+      Geometry.GetInIceGeometry()[OMKey(string_F,tube_F)] = amanda;
+      
+      amanda->SetPos(x_F * I3Units::m,
+		     y_F * I3Units::m,
+		     z_F * I3Units::m);
+      if (orientation_F == -1) amanda->SetOrientation(I3OMGeo::Down);
+      else if (orientation_F == 1) amanda->SetOrientation(I3OMGeo::Up);
+      amanda->SetArea(0.0284 * I3Units::m2);
+      amanda->SetRelativeQE(1.0);
+    }
+  
+  while(I3GeoInFile>>string_F>>tube_F>>x_F>>y_F>>z_F>>orientation_F)
+    {
+      if(tube_F<61)
+      {
+        I3OMGeoPtr icecube = I3OMGeoPtr(new I3OMGeo());
+        Geometry.GetInIceGeometry()[OMKey(string_F,tube_F)] = icecube;
+      
+        icecube->SetPos(x_F * I3Units::m,
+                        y_F * I3Units::m,
+		        z_F * I3Units::m);
+        icecube->SetOrientation(I3OMGeo::Down); 
+        icecube->SetArea(0.0444 * I3Units::m2);
+        icecube->SetRelativeQE(1.0);
+      }
+      else
+      {
+        I3StationMap &station_map = Geometry.GetIceTopGeometry().GetStationMap();
+        if(station_map.find(string_F)==station_map.end()) 
+        {
+          station_map[string_F] = I3StationGeoPtr(new I3StationGeo);
+          station_map[string_F]->push_back(I3SurfModuleGeoPtr(new I3SurfModuleGeo()));
+          station_map[string_F]->push_back(I3SurfModuleGeoPtr(new I3SurfModuleGeo()));
+        }
+
+        I3OMGeoPtr icecube = I3OMGeoPtr(new I3OMGeo());
+        switch(tube_F)
+        {
+          case 61:
+          case 62: (*(*station_map[string_F])[0])[OMKey(string_F, tube_F)] = icecube; break;
+          case 63:
+          case 64: (*(*station_map[string_F])[1])[OMKey(string_F, tube_F)] = icecube; break;
+          default: Fatal("Got a wrong tube number.");
+        }
+        icecube->SetPos(x_F * I3Units::m,
+                        y_F * I3Units::m,
+		        z_F * I3Units::m);
+        icecube->SetOrientation(I3OMGeo::Down); 
+        icecube->SetArea(0.0444 * I3Units::m2);
+        icecube->SetRelativeQE(1.0);
+      }
+    }
+  
+  AmaGeoInFile.close();
+  I3GeoInFile.close();
+  
+  //Integrate into DB Access part too?
+  log_warn("I3Db::FillFileGeometry: ATTENTION: No date set in header or date set to 0.");
+  
+  I3Time time;
+  time.SetModJulianTime(0,0,0.0);
+  Geometry.SetStartTime(time);
+}
+
