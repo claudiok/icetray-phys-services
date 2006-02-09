693393fe2c3fa662a48ebab953182f4c97089862
Philip Roth <aproth@icecube.umd.edu>
2006-02-09 15:28:42 +0000

    Adding a namespace that's capable of converting MC objects to their respective reco objects.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/branches/dc2@15496 16731396-06f5-0310-8873-f7f720988828

---
+++ private/phys-services/I3MC2Reco.cxx
@@ -0,0 +1,28 @@
+/**
+    copyright  (C) 2004
+    the icecube collaboration
+    $Id: I3MC2Reco.cxx 15487 2006-02-08 20:58:22Z dule $
+
+    @version $Revision: 1.1 $
+    @date $Date: 2006-02-08 15:58:22 -0500 (Wed, 08 Feb 2006) $
+    @author
+
+    @todo
+
+*/
+#include "phys-services/I3MC2Reco.h"
+
+using namespace std;
+
+
+//--------------------------------------------------------------
+void I3MC2Reco::mc2reco(const I3MCHit& src, //input
+			I3Hit& dst)         //output
+{
+  dst.SetTime( src.GetTime());
+  dst.SetID( src.GetHitID());
+
+  return;
+}
+
+//void I3MC2Reco::mc2reco(const I3MC
