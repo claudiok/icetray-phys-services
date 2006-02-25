9579ce22ee7ca6f09d6d74e53a028cf6d5c417dd
Erik Blaufuss <blaufuss@icecube.umd.edu>
2006-02-25 16:21:39 +0000

    Fixed this tests for v2.  Now tests I3DOMFunction calls
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@16314 16731396-06f5-0310-8873-f7f720988828

--- private/test/CalibrateStatusModuleTest.cxx
+++ private/test/CalibrateStatusModuleTest.cxx
@@ -1,9 +1,10 @@
 #include <I3Test.h>
 
-#warning broken
-#if 0
-#include "phys-services/source/I3CalibrateStatusModule.h"
-#include "dataclasses/status/I3MCRawDOMStatus.h"
+
+
+#include "dataclasses/status/I3DOMStatus.h"
+#include "dataclasses/calibration/I3DOMCalibration.h"
+#include "dataclasses/I3DOMFunctions.h"
 #include "dataclasses/I3Units.h"
 
 #include <sstream>
@@ -12,52 +13,50 @@
 using namespace std;
 
 // Test some of Calibrate Module functionality
-TEST_GROUP(CalibrateStatusModuleStatus);
+TEST_GROUP(CalibrateStatus);
 
 TEST(DoCalibTest1)
     {
-    I3MCRawDOMStatusPtr rawstatus(new I3MCRawDOMStatus());
-    I3CalibratedDOMStatusPtr calibratedstatus(new I3CalibratedDOMStatus());
-    I3DOMCalibrationPtr calib(new I3DOMCalibration());
+      I3DOMStatus rawstatus;
+
+      I3DOMCalibration calib;
     
-    //Set some values
-    rawstatus->SetPMTHV(1400*I3Units::V);
-    rawstatus->SetDACTriggerBias(850,0);
-    rawstatus->SetDACTriggerBias(850,1);
-    QuadraticFit qfit0,qfit1;
-    //Values from an actual DOMCAL file.
-    qfit0.quadFitA = 2.5858788;
-    qfit0.quadFitB = 0.013337472;
-    qfit0.quadFitC = NAN;  //Linear fit here
-    qfit1.quadFitA = 2.3853257;
-    qfit1.quadFitB = 0.014224272;
-    qfit1.quadFitC = NAN;  //Linear fit here
+      //Set some values
+      rawstatus.pmtHV = 1400*I3Units::V;
+      rawstatus.dacTriggerBias0 = 850;
+      rawstatus.dacTriggerBias1 = 850;
+      QuadraticFit qfit0,qfit1;
+      //Values from an actual DOMCAL file.
+      qfit0.quadFitA = 2.5858788;
+      qfit0.quadFitB = 0.013337472;
+      qfit0.quadFitC = NAN;  //Linear fit here
+      qfit1.quadFitA = 2.3853257;
+      qfit1.quadFitB = 0.014224272;
+      qfit1.quadFitC = NAN;  //Linear fit here
 
-    calib->SetATWDFreqFit(0,qfit0);
-    calib->SetATWDFreqFit(1,qfit1);
+      calib.SetATWDFreqFit(0,qfit0);
+      calib.SetATWDFreqFit(1,qfit1);
 
-    LinearFit hvgainfit;
-    hvgainfit.intercept = -15.1997;
-    hvgainfit.slope = 7.0842533;
-    
-    calib->SetHVGainFit(hvgainfit);
+      LinearFit hvgainfit;
+      hvgainfit.intercept = -15.1997;
+      hvgainfit.slope = 7.0842533;
+      
+      calib.SetHVGainFit(hvgainfit);
+      
+      double atwdrate0 =  ATWDSamplingRate(0,rawstatus,calib);
+      double atwdrate1 =  ATWDSamplingRate(1,rawstatus,calib);
+      double gain = PMTGain(rawstatus,calib);
+      double spemean = SPEMean(rawstatus,calib);
 
-    I3CalibrateStatusModule::DoTheCalibration(rawstatus,
-					      calibratedstatus,
-					      *calib);
+      //Compare the return values to values calculated by hand.
+      
+      ENSURE_DISTANCE(spemean/I3Units::pC*1.0E12,1.9632,
+		      0.001,"Failed to return the proper SPEMean value");
+      ENSURE_DISTANCE(atwdrate0*I3Units::microsecond, 278.455,
+		      0.001,"Failed to return proper ATWD-A sampling rate");
+      ENSURE_DISTANCE(atwdrate1*I3Units::microsecond,289.519,
+		      0.001,"Failed to return proper ATWD-B sampling rate");
+      
+      
+    }
 
-    //Compare the return values to values calculated by hand.
-
-    ENSURE_DISTANCE(1.9632,calibratedstatus->GetSPEMean()/I3Units::pC*1.0E12,
-		    0.001,"Failed to return the proper SPEMean value");
-    ENSURE_DISTANCE(278.455,
-		    calibratedstatus->GetSamplingRateA()*I3Units::microsecond,
-                    0.001,"Failed to return proper ATWD-A sampling rate");
-    ENSURE_DISTANCE(289.519,
- 		    calibratedstatus->GetSamplingRateB()*I3Units::microsecond,
-                    0.001,"Failed to return proper ATWD-B sampling rate");
-
-
-}
-
-#endif
