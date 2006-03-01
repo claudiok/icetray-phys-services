fdde98f825cfc7c98f143c44b57480c54bc02626
John Pretz <pretz@icecube.umd.edu>
2006-03-01 14:59:45 +0000

    example using empty streams
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@16580 16731396-06f5-0310-8873-f7f720988828

---
+++ resources/scripts/EmptySourceExample.py
@@ -0,0 +1,16 @@
+from I3Tray import *
+
+load("libphys-services")
+
+tray = I3Tray()
+
+tray.AddService("I3EmptyStreamsFactory","emptystreams")
+
+tray.AddModule("I3Muxer","muxer")
+
+#tray.AddModule("Dump","dump")
+
+tray.AddModule("TrashCan","trash")
+
+tray.Execute(10)
+tray.Finish()
