cb6e38fef02040d291c3909e65a24e465538db49
Alex Olivas <olivas@icecube.umd.edu>
2006-03-20 03:24:37 +0000

    I3GeometrySelector compiles with DC2.  Icetray test is hanging so I commented it out until tomorrow.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@17529 16731396-06f5-0310-8873-f7f720988828

---
+++ private/geo-selector/I3GeometrySelector.cxx
@@ -0,0 +1,120 @@
+/**
+ * copyright  (C) 2004
+ * the icecube collaboration
+ * $Id: I3GeometrySelector.cxx 2598 2005-02-04 17:18:56Z olivas $
+ *
+ * @file I3GeometrySelector.cxx
+ * @version $Revision: 1.8 $
+ * @date $Date: 2005-02-04 18:18:56 +0100 (Fri, 04 Feb 2005) $
+ * @author olivas
+ *
+ * This class shows how to add a Selection to the event.
+*/
+
+// headers for this selection
+#include "phys-services/geo-selector/I3GeometrySelector.h"
+#include "phys-services/geo-selector/GeoSelUtils.h"
+
+// other headers
+//  global header for all the IceTray stuff
+#include "icetray/I3TrayHeaders.h"
+/* headers for the dataclasses stuff */
+#include "dataclasses/geometry/I3Geometry.h"
+#include "dataclasses/OMKey.h"
+#include <iostream>
+#include <sstream>
+
+I3_MODULE(I3GeometrySelector);
+
+using namespace std;
+
+I3GeometrySelector::I3GeometrySelector(const I3Context& ctx) : 
+  I3Module(ctx),
+  stringsToUse_("-19:80"),
+  stringsToExclude_(""),
+  stationsToUse_("1:80"),
+  stationsToExclude_(""),
+  newGeometryName_("NewGeometry")
+{
+    AddOutBox("OutBox");
+
+    AddParameter("StringsToUse", 
+		 "The strings that should be included", 
+		 stringsToUse_);
+    AddParameter("StringsToExclude", 
+		 "The strings that should be excluded", 
+		 stringsToExclude_);
+    AddParameter("StationsToUse", 
+		 "The strings that should be included", 
+		 stationsToUse_);
+    AddParameter("StationsToExclude", 
+		 "The strings that should be excluded", 
+		 stationsToExclude_);
+    AddParameter("NewGeometryName", 
+		 "Name of the geometry that's going into the frame", 
+		 newGeometryName_);
+}
+
+I3GeometrySelector::~I3GeometrySelector() {
+}
+
+// transitions
+void I3GeometrySelector::Configure() {
+  GetParameter("StringsToUse",stringsToUse_);
+  GetParameter("StringsToExclude",stringsToExclude_);
+  GetParameter("StationsToUse",stationsToUse_);
+  GetParameter("StationsToExclude",stationsToExclude_);
+  GetParameter("NewGeometryName",newGeometryName_);
+
+  if(!geo_sel_utils::good_input(stringsToUse_)) 
+    log_fatal("couldn't parse %s",stringsToUse_.c_str());
+  if(!geo_sel_utils::good_input(stringsToExclude_)) 
+    log_fatal("couldn't parse %s",stringsToExclude_.c_str());
+  if(!geo_sel_utils::good_input(stationsToUse_)) 
+    log_fatal("couldn't parse %s",stationsToUse_.c_str());
+  if(!geo_sel_utils::good_input(stationsToExclude_)) 
+    log_fatal("couldn't parse %s",stationsToExclude_.c_str());
+
+  goodStrings_ = geo_sel_utils::make_good_strings(stringsToUse_, stringsToExclude_);
+  goodStations_ = geo_sel_utils::make_good_strings(stationsToUse_, stationsToExclude_);
+}
+
+void I3GeometrySelector::Geometry(I3FramePtr frame) {
+
+  log_debug("Entering Geometry method.");
+  
+  // Get the event information out of the Frame
+  I3GeometryConstPtr geoPtr = frame->Get<I3GeometryConstPtr>();
+  if(!geoPtr) log_fatal("Couldn't get geometry");
+
+  log_trace("***before*** geoPtr->omgeo.size(): %zu",geoPtr->omgeo.size());
+
+  I3Geometry new_geo; //making a new geometry
+
+  new_geo.startTime = geoPtr->startTime;
+  new_geo.endTime = geoPtr->endTime;
+
+  //erasing is too problematic
+  I3OMGeoMap::const_iterator iter;
+  for(iter = geoPtr->omgeo.begin();
+      iter != geoPtr->omgeo.end(); ++iter){
+    OMKey omkey = iter->first;
+    if(geoPtr->omgeo.count(omkey)>1) log_error("***how the hell did that happen?***");
+    if(geo_sel_utils::exists(omkey.GetString(),goodStrings_))
+      new_geo.omgeo[omkey] = iter->second;
+  }
+
+  I3StationGeoMap::const_iterator siter;
+  for(siter = geoPtr->stationgeo.begin();
+      siter != geoPtr->stationgeo.end(); ++siter){
+    int station = siter->first;
+    if(geoPtr->stationgeo.count(station)>1) log_error("***how the hell did that happen?***");
+    if(geo_sel_utils::exists(station,goodStrings_))
+      new_geo.stationgeo[station] = siter->second;
+  }
+
+  log_trace("***after*** geoPtr->omgeo().size(): %zu",geoPtr->omgeo.size());
+  log_trace("***after*** geoPtr->stationgeo.size(): %zu",geoPtr->stationgeo.size());
+
+  PushFrame(frame,"OutBox");  
+}
---
+++ private/test/I3GeoSelTestModule.cxx
@@ -0,0 +1,132 @@
+/**
+ * copyright  (C) 2004
+ * the icecube collaboration
+ * $Id: I3GeometrySelector.cxx 2598 2005-02-04 17:18:56Z olivas $
+ *
+ * @file I3GeometrySelector.cxx
+ * @version $Revision: 1.8 $
+ * @date $Date: 2005-02-04 18:18:56 +0100 (Fri, 04 Feb 2005) $
+ * @author olivas
+ *
+ * This class shows how to add a Selection to the event.
+*/
+
+// headers for this selection
+#include <I3Test.h>
+#include "phys-services/geo-selector/I3GeoSelTestModule.h"
+#include "phys-services/geo-selector/GeoSelUtils.h"
+
+// other headers
+//  global header for all the IceTray stuff
+#include "icetray/I3TrayHeaders.h"
+/* headers for the dataclasses stuff */
+#include "dataclasses/geometry/I3Geometry.h"
+#include "dataclasses/OMKey.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+I3GeoSelTestModule::I3GeoSelTestModule(const I3Context& ctx) : 
+  I3Module(ctx),
+  stringsToUse_("-19:80"),
+  stringsToExclude_(""),
+  stationsToUse_("1:80"),
+  stationsToExclude_(""),
+  newGeometryName_("NewGeometry")
+{
+    AddOutBox("OutBox");
+
+    AddParameter("StringsToUse", 
+		 "The strings that should be included", 
+		 stringsToUse_);
+    AddParameter("StringsToExclude", 
+		 "The strings that should be excluded", 
+		 stringsToExclude_);
+    AddParameter("StationsToUse", 
+		 "The stations that should be included", 
+		 stationsToUse_);
+    AddParameter("StationsToExclude", 
+		 "The stations that should be excluded", 
+		 stationsToExclude_);
+    AddParameter("NewGeometryName", 
+		 "Name of the geometry that's going into the frame", 
+		 newGeometryName_);
+}
+
+I3GeoSelTestModule::~I3GeoSelTestModule() {
+}
+
+// transitions
+void I3GeoSelTestModule::Configure() {
+  GetParameter("StringsToUse",stringsToUse_);
+  GetParameter("StringsToExclude",stringsToExclude_);
+  GetParameter("StationsToUse",stationsToUse_);
+  GetParameter("StationsToExclude",stationsToExclude_);
+  GetParameter("NewGeometryName",newGeometryName_);
+
+  if(!geo_sel_utils::good_input(stringsToUse_)) 
+    log_fatal("couldn't parse %s",stringsToUse_.c_str());
+  if(!geo_sel_utils::good_input(stringsToExclude_)) 
+    log_fatal("couldn't parse %s",stringsToExclude_.c_str());
+  if(!geo_sel_utils::good_input(stationsToUse_)) 
+    log_fatal("couldn't parse %s",stationsToUse_.c_str());
+  if(!geo_sel_utils::good_input(stationsToExclude_)) 
+    log_fatal("couldn't parse %s",stationsToExclude_.c_str());
+
+  cout<<"stringsToUse_"<<stringsToUse_<<endl;
+
+  goodStrings_ = geo_sel_utils::make_good_strings(stringsToUse_, stringsToExclude_);
+  goodStations_ = geo_sel_utils::make_good_strings(stationsToUse_, stationsToExclude_);}
+
+void I3GeoSelTestModule::Geometry(I3FramePtr frame) {
+
+  log_debug("Entering Geometry method.");
+  
+  // Get the event information out of the Frame
+  I3GeometryConstPtr geoPtr = frame->Get<I3GeometryConstPtr>();
+  if(!geoPtr) log_fatal("Couldn't get geometry");
+
+  //loop through the in ice geometry and make sure that
+  //1) All the DOMs that exist in the geometry are in goodStrings_
+  //2) None of the DOMs correspond to string in stringsToExclude_
+
+  log_trace("***before*** geoPtr->omgeo.size(): %zu",geoPtr->omgeo.size());
+
+  cout<<"goodStrings_.size(): "<<goodStrings_.size()<<endl;
+  for(vector<int>::iterator iter = goodStrings_.begin(); iter != goodStrings_.end(); ++iter)
+    cout<<*iter<<" ";
+  cout<<endl;
+
+  cout<<"goodStations_.size(): "<<goodStations_.size()<<endl;
+  for(vector<int>::iterator iter = goodStations_.begin(); iter != goodStations_.end(); ++iter)
+    cout<<*iter<<" ";
+  cout<<endl;
+
+  cout<<endl;
+  std::vector<int> exclude_list = geo_sel_utils::parse_string_list(stringsToExclude_);
+
+  I3OMGeoMap::const_iterator iter;
+  for(iter = geoPtr->omgeo.begin();
+      iter != geoPtr->omgeo.end(); ++iter){
+    OMKey omkey = iter->first;
+    cout<<omkey<<" ";
+    ENSURE(geo_sel_utils::exists(omkey.GetString(),goodStrings_));
+    ENSURE(!geo_sel_utils::exists(omkey.GetString(),exclude_list));
+  }
+
+  I3StationGeoMap::const_iterator siter;
+  for(siter = geoPtr->stationgeo.begin();
+      siter != geoPtr->stationgeo.end(); ++siter){
+    int station = siter->first;
+    cout<<station<<" ";
+    ENSURE(geo_sel_utils::exists(station,goodStrings_));
+    ENSURE(!geo_sel_utils::exists(station,exclude_list));
+  }
+  
+  PushFrame(frame,"OutBox");
+  
+  log_debug("Added selection.");
+}
+
+
