aeaa346d8b3a90030d78ef74c6a9910eb67737c9
Alex Olivas <olivas@icecube.umd.edu>
2006-03-29 22:19:46 +0000

    better tests for I3TestSource
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@18012 16731396-06f5-0310-8873-f7f720988828

--- private/test/TestSourceTests.cxx
+++ private/test/TestSourceTests.cxx
@@ -37,11 +37,34 @@ TEST(multiple_DOMLaunches_stress_test){
   I3Tray tray;
 
   boost::rand48 rng(static_cast<int>(time(0)));
-  boost::uniform_smallint<unsigned> nObjects_rng(4000,4800);
+  boost::uniform_smallint<unsigned> nObjects_rng(400,1000);
   unsigned nObjects = nObjects_rng(rng);
 
   tray.AddService("I3EmptyStreamsFactory","empty_streams")
     ("NFrames",1);
+  tray.AddModule("I3Muxer","muxer");
+  tray.AddModule("I3TestSource<I3DOMLaunch>","test_source")
+    ("OutputMap","ParticleMap")
+    ("NObjects",nObjects);
+  tray.AddModule("I3TestSourceTestModule","test_module")
+    ("MapName","ParticleMap")
+    ("NObjects",nObjects);
+  tray.AddModule("TrashCan","trash");
+
+  tray.Execute();
+  tray.Finish();
+}
+
+TEST(multiple_DOMLaunches_super_stress_test){
+
+  I3Tray tray;
+
+  boost::rand48 rng(static_cast<int>(time(0)));
+  boost::uniform_smallint<unsigned> nObjects_rng(4400,4800);
+  unsigned nObjects = nObjects_rng(rng);
+
+  tray.AddService("I3EmptyStreamsFactory","empty_streams")
+    ("NFrames",100);
   tray.AddModule("I3Muxer","muxer");
   tray.AddModule("I3TestSource<I3DOMLaunch>","test_source")
     ("OutputMap","ParticleMap")
