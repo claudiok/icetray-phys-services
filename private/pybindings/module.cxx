7a0e4a54a36ad257a5237284f754672af32e193d
Troy Straszheim <troy@icecube.umd.edu>
2008-07-24 06:37:38 +0000

    quietly load underlying cpp libraries.  This stops nasty-looking serialization assertion failure when an exported class double-registers itself with the archives
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@47509 16731396-06f5-0310-8873-f7f720988828

--- private/pybindings/module.cxx
+++ private/pybindings/module.cxx
@@ -20,6 +20,7 @@
 //
 
 #include <icetray/I3FrameObject.h>
+#include <icetray/load_project.h>
 
 using namespace boost::python;
 namespace bp = boost::python;
@@ -43,6 +44,8 @@ BOOST_PP_SEQ_FOR_EACH(I3_REGISTRATION_FN_DECL, ~, REGISTER_THESE_THINGS)
 
 BOOST_PYTHON_MODULE(phys_services)
 {
+  load_project("libphys-services", false);
+
   BOOST_PP_SEQ_FOR_EACH(I3_REGISTER, ~, REGISTER_THESE_THINGS);
 }
 
