5040d215f8c86ea179b0be8c1961280b06dc7443
Fabian Kislat <fabian.kislat@desy.de>
2010-05-25 19:58:26 +0000

    Forgot that file in my last commit
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@64313 16731396-06f5-0310-8873-f7f720988828

---
+++ private/pybindings/Converter.cxx
@@ -0,0 +1,40 @@
+/**
+ * copyright  (C) 2010
+ * The Icecube Collaboration
+ *
+ * $Id$
+ *
+ * @version $Revision$
+ * @date $LastChangedDate$
+ * @author Fabian Kislat <fabian.kislat@desy.de> $LastChangedBy$
+ */
+
+#include <tableio/converter/pybindings.h>
+#include "../converter/I3EventInfoConverter.h"
+#include "../converter/I3RecoInfoConverter.h"
+
+void register_Converter() {
+    I3CONVERTER_NAMESPACE(converter_limbo);
+
+    //typedef I3EventInfoConverter<I3RecoPulse> I3EventInfoConverterFromRecoPulses;
+    I3CONVERTER_EXPORT(I3EventInfoConverterFromRecoPulses,"Dumps event information calculated from I3RecoPulses");
+    typedef I3EventInfoConverter<I3RecoHit> I3EventInfoConverterFromRecoHit;
+    I3CONVERTER_EXPORT(I3EventInfoConverterFromRecoHit,"Dumps event information calculated from I3RecoHit");
+    typedef I3EventInfoConverter<I3MCHit> I3EventInfoConverterFromMCHit;
+    I3CONVERTER_EXPORT(I3EventInfoConverterFromMCHit,"Dumps event information calculated from I3MCHit");
+    typedef I3EventInfoConverter<I3DOMLaunch> I3EventInfoConverterFromDOMLaunch;
+    I3CONVERTER_EXPORT(I3EventInfoConverterFromDOMLaunch,"Dumps event information calculated from I3DOMLaunch");
+    typedef I3EventInfoConverter<I3Waveform> I3EventInfoConverterFromWaveform;
+    I3CONVERTER_EXPORT(I3EventInfoConverterFromWaveform,"Dumps event information calculated from I3Waveform");
+    
+    
+    // I3RecoInfoConverter has a non-default constructor
+    // don't register this converter, since registered converters need default constructors
+    bp::class_<I3RecoInfoConverter,                                      
+    boost::shared_ptr<I3RecoInfoConverter>,                 
+    bp::bases<I3Converter>,                      
+    boost::noncopyable >("I3RecoInfoConverter", 
+                         "calculates number of direct hits and icecube scale cut variables", 
+                         bp::init<std::string, int, int>())
+    ;
+};
