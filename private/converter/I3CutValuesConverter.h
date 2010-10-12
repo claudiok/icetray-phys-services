e9ac4b67296a138e405f1c5cb1caee6cba5cbaa7
Jakob van Santen <jvansanten@gmail.com>
2010-10-12 17:09:32 +0000

    Unbreak the phys-services build. Mea culpa.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@67452 16731396-06f5-0310-8873-f7f720988828

--- private/converter/I3CutValuesConverter.h
+++ private/converter/I3CutValuesConverter.h
@@ -13,8 +13,7 @@
 #include <phys-services/I3CutValues.h>
 
 class I3CutValuesConverter : public I3ConverterImplementation<I3CutValues > {
-public:
-    I3CutValuesConverter();
+
 private:
     I3TableRowDescriptionPtr CreateDescription(const I3CutValues& cv);
     size_t FillRows(const I3CutValues& cv, I3TableRowPtr rows);
