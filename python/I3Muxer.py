9f4620b453e614adbcad5233150bffecb5f953f8
Jakob van Santen <jvansanten@gmail.com>
2011-01-19 10:27:01 +0000

    Clean up I3Muxer wrapper allow constructor to take a file-list directly.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@70237 16731396-06f5-0310-8873-f7f720988828

--- python/I3Muxer.py
+++ python/I3Muxer.py
@@ -9,15 +9,47 @@
 # @date $LastChangedDate$
 # @author Jakob van Santen <vansanten@wisc.edu> Last changed by: $LastChangedBy$
 
-from icecube import icetray
+from icecube import icetray, dataio
 from icecube.phys_services import I3MuxerModule
 from icecube.interfaces import I3EventService, I3GeometryService, I3CalibrationService, I3DetectorStatusService, I3MetaService
+import types, operator
 
 class I3Muxer(I3MuxerModule):
-	"""A stand-alone wrapper for the I3Muxer module."""
+	"""
+A stand-alone wrapper for the I3Muxer module.
+
+The I3Muxer wrapper may be configured by explicitly passing the Event, Geometry,
+Calibration, and Status services, or by passing a list of I3 files to be read.
+
+Explicit services:
+
+	reader = dataio.I3ReaderService(['GCD.i3.gz', 'foo.i3.gz'], [], True)
+	muxer = phys_services.I3Muxer(reader)
+
+List of files (implicitly creates an I3ReaderService):
+
+	muxer = phys_services.I3Muxer(['GCD.i3.gz', 'foo.i3.gz'])
+
+List of lists of files (implicitly creates an I3MuxingReaderService):
+
+	muxer = phys_services.I3Muxer([['GCD.i3.gz'], ['run1.i3'], ['run2_part1.i3', 'run2_part2.i3']])
+
+	"""
 	def __init__(self, event_service, geo_service=None, calibration_service=None, status_service=None):
 		# Build up a fake context for module instantiation.
 		context = icetray.I3Context()
+		
+		# If the event service is not an I3EventService, try to construct one.
+		if not isinstance(event_service, I3EventService):
+			listlike = (types.ListType, types.TupleType)
+			if not isinstance(event_service, listlike):
+				raise TypeError, "event_service must be a list or an instance of I3EventService"
+			if reduce(operator.and_, map(lambda i: isinstance(i, listlike), event_service)):
+				# a list-of-lists: employ the muxing reader
+				event_service = dataio.I3MuxingReaderService(event_service, [], True)
+			else:
+				# vanilla reader
+				event_service = dataio.I3ReaderService(event_service, [], True)
 		
 		# Install services.
 		for ptr, klass in [(event_service, I3EventService),
@@ -48,12 +80,12 @@ class I3Muxer(I3MuxerModule):
 	def next(self):
 		if self.next_frame is None:
 			raise StopIteration
-		return pop_frame()
+		return self.pop_frame()
 		
 	def iterframes(self, stream = icetray.I3Frame.Physics):
 		while self.more():
 			frame = icetray.I3Frame()
-			while frame.GetStop != stream:
+			while frame.GetStop() != stream:
 				frame = self.next()
 			yield frame
 	
