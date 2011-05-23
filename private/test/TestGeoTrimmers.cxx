0d73d910b803290d72563e1a2da0e28d76b18d25
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-05-23 23:22:02 +0000

    Clean up compile errors left behind by others in r75484.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@75502 16731396-06f5-0310-8873-f7f720988828

--- private/test/TestGeoTrimmers.cxx
+++ private/test/TestGeoTrimmers.cxx
@@ -34,18 +34,18 @@ static I3RecoPulseSeriesMapPtr rpsm() {
   // Hits on DOM's 1-5 only
   I3RecoPulseSeriesMapPtr rp(new I3RecoPulseSeriesMap);
   I3RecoPulse p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11;
-  p0.SetTime(1650);   p0.SetWidth(100);  p0.SetID(0);   //close to #1       //coincidence with #1
-  p1.SetTime(1850);   p1.SetWidth(100);  p1.SetID(1);   //close to #0       //coincidence with #0
-  p2.SetTime(0);      p2.SetWidth(100);  p2.SetID(2);   //close to #3       //coincidence with #3
-  p3.SetTime(550);    p3.SetWidth(0);    p3.SetID(3);   //close to #2, #4   //coincidence with #2, #4
-  p4.SetTime(800);    p4.SetWidth(0);    p4.SetID(4);   //close to #3       //coincidence with #3
-  p5.SetTime(3500);   p5.SetWidth(100);  p5.SetID(5);   //isolated          //coincidence with #6, #7
-  p6.SetTime(3500);   p6.SetWidth(100);  p6.SetID(6);   //close to #7, #8   //coincidence with #5, #7
-  p7.SetTime(3500);   p7.SetWidth(100);  p7.SetID(7);   //close to #6, #8   //coincidence with #5, #6
-  p8.SetTime(3500);   p8.SetWidth(100);  p8.SetID(8);   //close to #6, #7   //isolated
-  p9.SetTime(8000);   p9.SetWidth(3000); p9.SetID(9);   //close to #10, #11 //coincidence with #10, #11
-  p10.SetTime(9000);  p10.SetWidth(100); p10.SetID(10); //close to #9, #11  //coincidence with #9, #11
-  p11.SetTime(9500);  p11.SetWidth(100); p11.SetID(11); //close to #9, #10  //coincidence with #9, #10
+  p0.SetTime(1650);   p0.SetWidth(100);  //close to #1       //coincidence with #1
+  p1.SetTime(1850);   p1.SetWidth(100);  //close to #0       //coincidence with #0
+  p2.SetTime(0);      p2.SetWidth(100);  //close to #3       //coincidence with #3
+  p3.SetTime(550);    p3.SetWidth(0);    //close to #2, #4   //coincidence with #2, #4
+  p4.SetTime(800);    p4.SetWidth(0);    //close to #3       //coincidence with #3
+  p5.SetTime(3500);   p5.SetWidth(100);  //isolated          //coincidence with #6, #7
+  p6.SetTime(3500);   p6.SetWidth(100);  //close to #7, #8   //coincidence with #5, #7
+  p7.SetTime(3500);   p7.SetWidth(100);  //close to #6, #8   //coincidence with #5, #6
+  p8.SetTime(3500);   p8.SetWidth(100);  //close to #6, #7   //isolated
+  p9.SetTime(8000);   p9.SetWidth(3000); //close to #10, #11 //coincidence with #10, #11
+  p10.SetTime(9000);  p10.SetWidth(100); //close to #9, #11  //coincidence with #9, #11
+  p11.SetTime(9500);  p11.SetWidth(100); //close to #9, #10  //coincidence with #9, #10
   OMKey dom1(1,1);
   OMKey dom2(1,2);
   OMKey dom3(2,63);
