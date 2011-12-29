49e88fc571551dcfc199ba164d758de7cadbd0d9
Nathan Whitehorn <nwhitehorn@icecube.wisc.edu>
2011-12-29 21:18:21 +0000

    Allow use of GSL random number distribution with all I3RandomService objects, and simplify the implementation of a random number generator by providing default implementations of every method except Uniform(). The GSL distribution functions are very handy both in variety and for interfacing with C APIs (photospline, photonics).
    Note: the way the GSL bridge return type works is incredibly hacky for
    very stupid reasons involving the impossibility to forward-declare
    anonymous structs. Suggestions appreciated.
    
    
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@83461 16731396-06f5-0310-8873-f7f720988828

--- public/phys-services/I3RandomService.h
+++ public/phys-services/I3RandomService.h
@@ -22,6 +22,8 @@
 #include <icetray/I3DefaultName.h>
 #include <icetray/I3FrameObject.h>
 
+struct gsl_rng_wrap;
+
 /**
  * @brief This class provides an interface to random numbers suitable for use
  * by icetray modules.
@@ -30,6 +32,14 @@
  * reflect ROOT's TRandom interface.  Indeed TRandom could be used to 
  * trivially fill this interface
  *
+ * Only one function here is required: Uniform(x1, x2)
+ * The rest have default implementations in terms of it. More complicated
+ * random number distributions than provided here can be obtained using
+ * the GSL bridge (GSLRng()).
+ * 
+ * NOTE: when using the GSL bridge, this file MUST be included AFTER
+ * gsl/gsl_rng.h
+ *
  */
 
 class I3RandomService {
@@ -37,42 +47,42 @@ class I3RandomService {
   /**
    * constructor
    */
-  I3RandomService(){}
+  I3RandomService();
 
   /**
    * destructor
    */ 
-  virtual ~I3RandomService(){};
+  virtual ~I3RandomService();
 
   /**
    * a number drawn from a binomial distribution
    */
-  virtual int Binomial(int ntot, double prob)=0;
+  virtual int Binomial(int ntot, double prob);
 
   /**
    * A number from an Exponential distribution
    */
-  virtual double Exp(double tau)=0;
+  virtual double Exp(double tau);
 
   /**
    * An integer drawn uniformly from [0,imax)
    */
-  virtual unsigned int Integer(unsigned int imax)=0;
+  virtual unsigned int Integer(unsigned int imax);
 
   /**
    * An integer drawn from a Poisson distribution
    */
-  virtual int Poisson(double mean)=0;
+  virtual int Poisson(double mean);
 
   /**
    * A number drawn from a Poisson distribution, as a double
    */
-  virtual double PoissonD(double mean)=0;
+  virtual double PoissonD(double mean);
 
   /**
    * a double drawn from a uniform distribution (0,x1)
    */
-  virtual double Uniform(double x1 = 1)=0;
+  virtual double Uniform(double x1 = 1);
 
   /**
    * a double drawn from a uniform distribution (x1,x2)
@@ -83,12 +93,20 @@ class I3RandomService {
    * a double drawn from a Gaussian distribution with given
    * mean and standard deviation
    */
-  virtual double Gaus(double mean,double stddev) = 0;
+  virtual double Gaus(double mean,double stddev);
+
+  /**
+   * a gsl_rng that wraps this random service, for use with
+   * GSL random samplers and C APIs
+   */
+  const gsl_rng_wrap *GSLRng() const;
 
  private:
   // copy and assignment private
   I3RandomService(const I3RandomService&);
   const I3RandomService& operator=(const I3RandomService&);
+
+  gsl_rng_wrap *gsl_;
 
   SET_LOGGER("I3RandomService");
 
@@ -98,3 +116,9 @@ I3_DEFAULT_NAME(I3RandomService);
 I3_POINTER_TYPEDEFS(I3RandomService);
 
 #endif //I3RANDOMSERVICE_H
+
+#if defined(__GSL_RNG_H__) && !defined(I3_GSL_RNG_WRAP)
+#define I3_GSL_RNG_WRAP
+struct gsl_rng_wrap : public gsl_rng {};
+#endif
+
