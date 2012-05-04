085fc8d68abda86edd54082f1962412cd1c29d63
Alex Olivas <olivas@icecube.umd.edu>
2012-05-04 17:19:19 +0000

    reverting back to loading projects manually.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@87951 16731396-06f5-0310-8873-f7f720988828

--- private/pybindings/module.cxx
+++ private/pybindings/module.cxx
@@ -20,6 +20,7 @@
 //
 
 #include <icetray/I3FrameObject.h>
+#include <icetray/load_project.h>
 
 using namespace boost::python;
 namespace bp = boost::python;
@@ -44,6 +45,8 @@ BOOST_PP_SEQ_FOR_EACH(I3_REGISTRATION_FN_DECL, ~, REGISTER_THESE_THINGS)
 
 BOOST_PYTHON_MODULE(phys_services)
 {
+
+  load_project("phys-services", false); 
   import("icecube.interfaces");
 
   BOOST_PP_SEQ_FOR_EACH(I3_REGISTER, ~, REGISTER_THESE_THINGS);
