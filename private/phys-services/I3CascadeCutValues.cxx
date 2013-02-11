claudiok/icetray-phys-services

commit 828510c5f7ba4e09a3abd80e6dd8a8921a94287e

let's try this again...
git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@95024 16731396-06f5-0310-8873-f7f720988828

private/phys-services/I3CutsModule.cxx
@@ -11,6 +11,14 @@
 #include "phys-services/I3CutValues.h"
 #include "phys-services/I3CascadeCutValues.h"
 
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/foreach.hpp>
+
+using boost::algorithm::split;
+using boost::algorithm::is_any_of;
+
 using namespace std;
 using namespace I3Units;
 
@@ -20,28 +28,31 @@ I3_MODULE(I3CutsModule);
 I3CutsModule::I3CutsModule(const I3Context& ctx) : I3ConditionalModule(ctx)
 {
   AddOutBox("OutBox");
-  
+
   particleName_ = "";
   AddParameter("ParticleNames", "Name of I3Particle to be used for cut calc"
-	       ,particleName_);
+               ,particleName_);
+
+  AddParameter("ParticleNameList", "List of I3Particle names to be used for cut calc"
+               ,particleNameList_);
 
   hitsName_ = "";
   AddParameter("HitsName", "Name of the hit series to be used for cut calc"
-	       ,hitsName_);
+               ,hitsName_);
 
   pulsesName_ = "";
   AddParameter("PulsesName", "Name of the pulse series to be used for cut calc"
-	       ,pulsesName_);
+               ,pulsesName_);
 
   nameTag_ = "";
   AddParameter("NameTag", "Tag to add to name of output object"
-	       ,nameTag_);
+               ,nameTag_);
 
   timeRange_.resize(2);
   timeRange_[0] = I3Constants::dt_window_l;
   timeRange_[1] = I3Constants::dt_window_h;
   AddParameter("DirectHitsTimeRange","Time range for calculating direct hits"
-	       ,timeRange_);
+               ,timeRange_);
 
 }
 
@@ -49,16 +60,33 @@ I3CutsModule::I3CutsModule(const I3Context& ctx) : I3ConditionalModule(ctx)
 //--------------------------------------------------------------
 void I3CutsModule::Configure()
 {
-  GetParameter("ParticleNames",particleName_);   
+  GetParameter("ParticleNames",particleName_);
+  GetParameter("ParticleNameList",particleNameList_);
   GetParameter("HitsName",hitsName_);
   GetParameter("PulsesName",pulsesName_);
   GetParameter("NameTag",nameTag_);
   GetParameter("DirectHitsTimeRange",timeRange_);
 
-  if (hitsName_.empty() && pulsesName_.empty()) 
+  if (hitsName_.empty() && pulsesName_.empty())
     log_fatal("Either 'HitsName' or 'PulsesName' parameter HAS to be set!");
-  if (!hitsName_.empty() && !pulsesName_.empty()) 
+  if (!hitsName_.empty() && !pulsesName_.empty())
     log_fatal("Can't set both 'HitsName' and 'PulsesName' parameters!");
+
+  if (!particleName_.empty() && !particleNameList_.empty()){
+    log_warn("Parameters passed to both 'ParticleNames' and 'ParticleNameList'");
+    log_warn("Please update your script to use 'ParticleNameList' as 'ParticleNames' is deprecated.");
+    log_warn("For now the lists will be merged.");
+  }
+
+  if (!particleName_.empty()){
+    log_warn("This parameter is deprecated. From now on please pass a "
+             "list of string names to 'ParticleNameList'.");
+    std::vector<std::string> particleNames;
+    split(particleNames, particleName_, is_any_of(":,;"));
+
+    BOOST_FOREACH(std::string& name, particleNames)
+      particleNameList_.push_back(name);
+  }
 }
 
 
@@ -69,110 +97,69 @@ void I3CutsModule::Physics(I3FramePtr frame)
   const I3Geometry& geometry = frame->Get<I3Geometry>();
 
   //---Get hit map......
-  I3RecoPulseSeriesMapConstPtr pulsemap = 
+  I3RecoPulseSeriesMapConstPtr pulsemap =
     frame->Get<I3RecoPulseSeriesMapConstPtr>(pulsesName_);
 
   if (!pulsemap) {
-    log_error("The chosen reaodut is not present.");
+    log_error("The chosen reaodut '%s' is not present.",pulsesName_.c_str());
     PushFrame(frame,"OutBox");
     return;
   }
-  //if the user has specified the I3Particles to process, process them...
-  if(particleName_!=""){
-    //---Get all particle names......
-    std::set<std::string> particleNames = I3Functions::ParseString(particleName_);
-    //iterate over the set of strings to see if the frame has an object by that name...
-    for (std::set<std::string>::const_iterator sit = particleNames.begin(); sit!=particleNames.end(); ++sit) {
-      std::string name = *sit;
-      if(frame->Has(name)){
-	
-	I3ParticleConstPtr particle = frame->Get<I3ParticleConstPtr>(name);
-	if (!particle) continue;
-	log_debug("Found I3Particle: '%s'",name.c_str());
-	log_trace("%s",ToString(particle).c_str());
-	
-	log_debug(" ---> calculating cuts for I3Particle '%s'...", name.c_str());
-	if(particle->IsTrack()){
-	  
-	  log_debug(" ---> I3Particle '%s' is a track, so proceeding accordingly...", name.c_str());
-	  I3CutValuesPtr cuts(new I3CutValues());
-	  
-	  if (pulsemap)
-	    cuts->Calculate(*particle,geometry,*pulsemap,timeRange_[0],timeRange_[1]);
-	  
-	  frame->Put(name+"Cuts"+nameTag_, cuts);
-	  log_debug("%s",ToString(cuts).c_str());
-	  
-	} else if(particle->IsCascade()){
-	  
-	  log_debug(" ---> I3Particle '%s' is a cascade, so proceeding accordingly...", name.c_str());
-	  I3CascadeCutValuesPtr cuts(new I3CascadeCutValues());
-	  
-	  if (pulsemap)
-	    cuts->Calculate(*particle,geometry,*pulsemap,timeRange_[0],timeRange_[1]);
-	  
-	  frame->Put(name+"Cuts"+nameTag_, cuts);
-	  log_debug("%s",ToString(cuts).c_str());
-	  
-	} else {
-	  //this is probably just a failed fit; if not, something is really screwy...
-	  if(particle->GetFitStatusString()=="OK"){
-	    log_debug("This I3Particle is neither a track nor a cascade!!");
-	  } else {
-	    log_debug("Looks like the fit for this I3Particle failed...");
-	  }
-	}
-      }
-    }
-  }
-  //otherwise particleName_ is empty, so process all I3Particles in the frame...
-  else {
 
+  //if the user did not specify then process all particles in the frame.
+  std::vector<std::string> particleNames(particleNameList_);
+  //if the user has specified the I3Particles to process, process them...
+  //otherwise particleName_ is empty, so process all I3Particles in the frame...
+  if(particleNames.empty()){
     for (I3Frame::typename_iterator iter = frame->typename_begin();
-	 iter != frame->typename_end();
-	 iter++) {
+         iter != frame->typename_end();
+         iter++) {
 
       std::string name = iter->first;
       std::string type = iter->second;
-      
-      if(type=="I3Particle"){
-      
-	I3ParticleConstPtr particle = dynamic_pointer_cast<const I3Particle>(frame->Get<I3FrameObjectConstPtr>(name));
-	
-	log_debug(" ---> calculating cuts for I3Particle '%s'...", name.c_str());
-	if(particle->IsTrack()){
-	  
-	  log_debug(" ---> I3Particle '%s' is a track, so proceeding accordingly...", name.c_str());
-	  I3CutValuesPtr cuts(new I3CutValues());
-	  
-	  if (pulsemap)
-	    cuts->Calculate(*particle,geometry,*pulsemap,timeRange_[0],timeRange_[1]);
-	  
-	  frame->Put(name+"Cuts"+nameTag_, cuts);
-	  log_debug("%s",ToString(cuts).c_str());
-	  
-	} else if(particle->IsCascade()){
-	  
-	  log_debug(" ---> I3Particle '%s' is a cascade, so proceeding accordingly...", name.c_str());
-	  I3CascadeCutValuesPtr cuts(new I3CascadeCutValues());
-	  
-	  if (pulsemap)
-	    cuts->Calculate(*particle,geometry,*pulsemap,timeRange_[0],timeRange_[1]);
-	  
-	  frame->Put(name+"Cuts"+nameTag_, cuts);
-	  log_debug("%s",ToString(cuts).c_str());
-	  
-	} else {
-	  //this is probably just a failed fit; if not, something is really screwy...
-	  if(particle->GetFitStatusString()=="OK"){
-	    log_debug("This I3Particle is neither a track nor a cascade!!");
-	  } else {
-	    log_debug("Looks like the fit for this I3Particle failed...");
-	  }
-	}
+
+      if(type=="I3Particle") particleNames.push_back(name);
+    }
+  }
+
+  //iterate over the set of strings to see if the frame has an object by that name...
+  for (std::vector<std::string>::const_iterator sit = particleNameList_.begin(); 
+       sit!=particleNameList_.end(); ++sit) {
+    std::string name = *sit;
+
+    I3ParticleConstPtr particle = frame->Get<I3ParticleConstPtr>(name);
+    if (!particle) continue;
+
+    log_debug("Found I3Particle: '%s'",name.c_str());
+    log_trace("%s",AsXML(particle).c_str());
+    log_debug(" ---> calculating cuts for I3Particle '%s'...", name.c_str());
+
+    I3CutValuesBasePtr cuts;
+    if(particle->IsTrack()){
+      log_debug(" ---> I3Particle '%s' is a track, so proceeding accordingly...", 
+                name.c_str());
+      cuts = I3CutValuesPtr(new I3CutValues());
+    }else if(particle->IsCascade()){
+      log_debug(" ---> I3Particle '%s' is a cascade, so proceeding accordingly...", 
+                name.c_str());
+      cuts = I3CascadeCutValuesPtr(new I3CascadeCutValues());
+    } else {
+      //this is probably just a failed fit; if not, something is really screwy...
+      if(particle->GetFitStatusString()=="OK"){
+        log_debug("This I3Particle is neither a track nor a cascade!!");
+      } else {
+        log_debug("Looks like the fit for this I3Particle failed...");
       }
+    }
+
+    if(!cuts){
+      cuts->Calculate(*particle,geometry,*pulsemap,timeRange_[0],timeRange_[1]);
+
+      frame->Put(name+"Cuts"+nameTag_, cuts);
+      log_debug("%s",AsXML(cuts).c_str());
     }
   }
 
   PushFrame(frame,"OutBox");
 }
+

public/phys-services/I3CutsModule.h
@@ -9,13 +9,12 @@
 
 #include <icetray/I3ConditionalModule.h>
 #include "icetray/I3Tray.h"
-#include "phys-services/I3Functions.h"
 #include "dataclasses/physics/I3Particle.h"
 
 
 /**
  * @brief
- * 
+ *
  */
 class I3CutsModule : public I3ConditionalModule
 {
@@ -25,9 +24,9 @@ public:
   ~I3CutsModule() { }
   void Configure();
   void Physics(I3FramePtr frame);
-  
+
 private:
-  
+
   I3CutsModule();
   I3CutsModule(const I3CutsModule& source);
   I3CutsModule& operator=(const I3CutsModule& source);
@@ -40,19 +39,24 @@ private:
   std::string particleName_;
 
   /**
+   * List of I3Particle names that the module writes to trees.
+   */
+  std::vector<std::string> particleNameList_;
+
+  /**
    * Parameter: name of the hit series map to use in the calculation of the
    * cut parameters.
    */
   std::string hitsName_;
 
   /**
-   * Parameter: name of the pulse series map to use in the calculation of 
+   * Parameter: name of the pulse series map to use in the calculation of
    * the cut parameters.
    */
   std::string pulsesName_;
 
   /**
-   * Parameter: Optional tag for the name of the object inserted by this module 
+   * Parameter: Optional tag for the name of the object inserted by this module
    * into the frame.
    */
   std::string nameTag_;

CMakeLists.txt
@@ -52,7 +52,6 @@ i3_add_library(phys-services
   private/phys-services/I3EventCounter.cxx
   private/phys-services/I3FileOMKey2MBID.cxx
   private/phys-services/I3FileOMKey2MBIDFactory.cxx
-  private/phys-services/I3Functions.cxx
   private/phys-services/I3GCDAuditor.cxx
   private/phys-services/I3RandomService.cxx
   private/phys-services/I3GSLRandomService.cxx
@@ -108,7 +107,6 @@ i3_test_executable(test
   private/test/ContainmentSizeTest.cxx
   private/test/GeometrySelectorTests.cxx
   private/test/I3CutsTest.cxx
-  private/test/I3FunctionTest.cxx
   private/test/I3GeoSelTestModule.cxx
   private/test/I3ScaleCalculatorTest.cxx
   private/test/I3XMLOMKey2MBIDTest.cxx

private/phys-services/I3Cuts.cxx
@@ -81,13 +81,6 @@ void CutsCalcImpl(const I3Particle& track, const I3Geometry& geometry,
 
       // calculate projections of hits onto track...
       Nhit++; // keep track of total hits
-      /*
-      I3Position pos(ompos);
-      pos.ShiftCoordSystem(track.GetPos());
-      pos.RotateZ(-track.GetDir().CalcPhi());
-      pos.RotateY(-track.GetDir().CalcTheta());
-      double dist = pos.GetZ();
-      */
       // Replace this with the function version
       double dist = DistanceAlongTrack(track, ompos);
       log_trace("dist: %f",dist);

private/phys-services/I3Functions.cxx
@@ -1,41 +1,0 @@
-#include "phys-services/I3Functions.h"
-#include "icetray/I3TrayHeaders.h"
-#include <glob.h>
-
-std::set<std::string> I3Functions::ParseString(const std::string &bigString)
-{
-  std::string current;
-  std::set<std::string> stringSet;
-  std::string::const_iterator iter;
-  for(iter = bigString.begin(); iter != bigString.end(); iter++) {
-    if(*iter==':' || *iter==',' || *iter==';') {
-      stringSet.insert(current);
-      current.clear();
-    }
-    else {
-      current.push_back(*iter);
-    }
-  }
-  if(!current.empty()) stringSet.insert(current);
-  return stringSet;
-}
-
-std::vector<std::string> I3Functions::ParseFilename(const std::string &bigFilename)
-{
-  glob_t                      g;
-  int                         globval;
-  int                         globflag=0;
-  std::set<std::string>       string_set=ParseString(bigFilename);
-  std::set<std::string>::const_iterator iter;
- 
-  for(iter=string_set.begin(); iter!=string_set.end(); iter++)
-  {
-    globval = glob((*iter).c_str(), globflag, 0, &g);
-    if(globflag==0) globflag=GLOB_APPEND;
-    if(globval!=0) log_fatal("glob() returned an error while checking %s",(*iter).c_str());
-  }  
-
-  std::vector<std::string> to_return(g.gl_pathv, g.gl_pathv+g.gl_pathc);
-  globfree(&g);
-  return(to_return);
-}

private/phys-services/Utility.cxx
@@ -1,3 +1,8 @@
+/**
+ * $Id$
+ * (c) 2005 IceCube Collaboration
+ */
+
 #include <string>
 #include "icetray/serialization.h"
 #include "dataclasses/Utility.h"
@@ -11,90 +16,25 @@
 #include "dataclasses/physics/I3Waveform.h"
 #include <boost/foreach.hpp>
 
-std::string ToString(shared_ptr<const I3FrameObject> obj)
-{
-  std::ostringstream outstring;
-  boost::archive::xml_oarchive ar(outstring);
-  ar << make_nvp(I3::name_of(typeid(*obj)).c_str(), obj);
-  return outstring.str();
-}
-
-std::string ToString(I3FrameObject* obj)
-{
-  std::ostringstream outstring;
-  boost::archive::xml_oarchive ar(outstring);
-  shared_ptr<I3FrameObject> ptr = shared_ptr<I3FrameObject>(new I3FrameObject(*obj));
-  ar << make_nvp(I3::name_of(typeid(*obj)).c_str(), ptr);
-  return outstring.str();
-}
-
-shared_ptr<I3FrameObject> Clone(shared_ptr<const I3FrameObject> ptr)
-{
-  std::ostringstream oss;
-  boost::archive::portable_binary_oarchive boa(oss);
-  boa << make_nvp("obj", ptr);
-  shared_ptr<I3FrameObject> newptr;
-  std::istringstream iss;
-  iss.str(oss.str());
-  boost::archive::portable_binary_iarchive bia(iss);
-  bia >> make_nvp("obj", newptr);
-  return newptr;
-}
-
-I3FrameObject* Clone(I3FrameObject* ptr)
-{
-  std::ostringstream oss;
-  boost::archive::portable_binary_oarchive boa(oss);
-  boa << make_nvp("obj", ptr);
-  shared_ptr<I3FrameObject> newptr;
-  std::istringstream iss;
-  iss.str(oss.str());
-  boost::archive::portable_binary_iarchive bia(iss);
-  bia >> make_nvp("obj", newptr);
-  return newptr.get();
-}
-
-void Copy(shared_ptr<const I3FrameObject> oldp,shared_ptr<I3FrameObject>& newp)
-{
-  std::ostringstream oss;
-  boost::archive::portable_binary_oarchive boa(oss);
-  boa << make_nvp("obj", oldp);
-  std::istringstream iss;
-  iss.str(oss.str());
-  boost::archive::portable_binary_iarchive bia(iss);
-  bia >> make_nvp("obj", newp);
-}
-
-void Copy(shared_ptr<const I3FrameObject> oldp, I3FrameObject* newp)
-{
-  std::ostringstream oss;
-  boost::archive::portable_binary_oarchive boa(oss);
-  boa << make_nvp("obj", oldp);
-  std::istringstream iss;
-  iss.str(oss.str());
-  boost::archive::portable_binary_iarchive bia(iss);
-  bia >> make_nvp("obj", newp);
-}
-
 double GetCharge(const I3RecoHit& mchit)
-{ 
-  return 1.; 
+{
+  return 1.;
 }
 
 double GetCharge(const I3MCHit& mchit)
-{ 
-  return 1.; 
+{
+  double w(mchit.GetWeight());
+  return isnan(w) ? 1. : mchit.GetWeight();
 }
 
 double GetCharge(const I3RecoPulse& pulse)
 {
-  //return (pulse.GetCharge() >= 2.0) ? pulse.GetCharge() : 1; 
-  return pulse.GetCharge(); 
+  return pulse.GetCharge();
 }
 
 double GetCharge(const I3DOMLaunch& launch)
-{ 
-  return NAN; 
+{
+  return NAN;
 }
 
 double GetCharge(const I3Waveform& wf)
@@ -104,7 +44,7 @@ double GetCharge(const I3Waveform& wf)
   for (iter=wf.GetWaveform().begin(); iter!=wf.GetWaveform().end(); iter++) {
     charge += *iter;
   }
-  return charge/I3Units::mV; 
+  return charge/I3Units::mV;
 }
 
 double GetCharge(const std::vector<I3RecoHit>& hit_series)
@@ -124,21 +64,21 @@ double GetCharge(const std::vector<I3RecoPulse>& pulse_series)
 
 // Functions to get time from either RecoPulse or MCHit.
 double GetTime(const I3MCHit& mchit)
-{ 
-  return mchit.GetTime(); 
+{
+  return mchit.GetTime();
 }
 
 double GetTime(const I3RecoPulse& pulse)
 {
-  return pulse.GetTime(); 
+  return pulse.GetTime();
 }
 
 double GetTime(const I3DOMLaunch& launch)
-{ 
-  return launch.GetStartTime(); 
+{
+  return launch.GetStartTime();
 }
 
 double GetTime(const I3Waveform& wf)
-{ 
-  return wf.GetStartTime(); 
+{
+  return wf.GetStartTime();
 }

private/test/I3CutsTest.cxx
@@ -7,9 +7,20 @@
 #include "dataclasses/physics/I3RecoPulse.h"
 #include "dataclasses/geometry/I3Geometry.h"
 
-using namespace I3Calculator;
-using namespace I3Cuts;
-using namespace I3Constants;
+using I3Calculator::CherenkovTime;
+using I3Cuts::ContainmentVolumeSize;
+using I3Cuts::CylinderSize;
+using I3Cuts::ContainmentAreaSize;
+using I3Cuts::CMPolygon;
+using I3Cuts::PutPointsInOrder;
+using I3Cuts::IntersectionOfLineAndPlane;
+using I3Cuts::SmoothDir;
+using I3Cuts::SmoothAll;
+using I3Cuts::Ldir;
+using I3Cuts::Ndir;
+using I3Constants::theta_cherenkov;
+using I3Constants::c_ice;
+using I3Constants::c;
 
 I3Geometry CalcGeom(std::vector<I3Position> pos)
 {

public/phys-services/I3CascadeCutValues.h
@@ -8,11 +8,12 @@
 #ifndef I3CASCADECUTVALUES_H
 #define I3CASCADECUTVALUES_H
 
-#include <dataclasses/I3Constants.h> 
-#include "dataclasses/I3Position.h"
+#include <phys-services/I3CutValuesBase.h>
+#include <dataclasses/I3Constants.h>
+#include <dataclasses/I3Position.h>
 
 template <typename Key, typename Value> struct I3Map;
-class OMKey; 
+class OMKey;
 class I3Particle;
 class I3RecoPulse;
 class I3Geometry;
@@ -21,7 +22,7 @@ class I3Geometry;
 /**
  * @brief A class to store the basic hit information for cascades from the event
  */
-class I3CascadeCutValues : public I3FrameObject
+class I3CascadeCutValues : public I3CutValuesBase
 {
  public:
   int Nchan;
@@ -43,11 +44,11 @@ class I3CascadeCutValues : public I3FrameObject
     Nlate(-1)
     { cog.NullPos(); };
 
-  void Calculate(const I3Particle& vertex, 
-		 const I3Geometry& geometry, 
-		 const I3Map< OMKey, std::vector< I3RecoPulse> >& pulsemap,
-		 const double& begTWindow = I3Constants::dt_window_l,
-		 const double& endTWindow = I3Constants::dt_window_h);
+    void Calculate(const I3Particle& vertex,
+                   const I3Geometry& geometry,
+                   const I3Map< OMKey, std::vector< I3RecoPulse> >& pulsemap,
+                   const double& begTWindow = I3Constants::dt_window_l,
+                   const double& endTWindow = I3Constants::dt_window_h);
 
   virtual ~I3CascadeCutValues();
 

commit 4e2fe453e4f2f335eba8c5cbb14d25b472c5b951

Bring forward I3CutsValues fixes from R98794
Here's the summary.

Attempt to fix breakage caused by r95024-95026:
Proceed to use a pointer only when it is non-NULL, rather than the reverse.
Fix serialization for I3CutsValues; however, previous versions of I3CutsValues
did not store a version number. As such, old versions of the code have no
safety check to ensure that the version being deserialized is one they
understand, so when new data is read by the old code silent corruption will
occur.

I also included the latest changes to the GCDAuditor for overall happy
feelings (or paranoia) when checking GCD files.


git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/releases/V13-01-00@98851 16731396-06f5-0310-8873-f7f720988828

private/phys-services/I3CascadeCutValues.cxx
@@ -20,10 +20,18 @@ void I3CascadeCutValues::Calculate(const I3Particle& vertex,
 
 I3CascadeCutValues::~I3CascadeCutValues() { }
 
+static const unsigned int current_i3cascadecutvalues_version=1;
+
 template <class Archive>
 void I3CascadeCutValues::serialize(Archive& ar, unsigned version)
 {
-  ar & make_nvp("I3FrameObject", base_object<I3FrameObject>(*this));
+  if (version>current_i3cascadecutvalues_version)
+    log_fatal("Attempting to read version %u from file but running version %u of I3CascadeCutValues class.",
+              version,current_i3cascadecutvalues_version);
+  if (version<1)
+    ar & make_nvp("I3FrameObject", base_object<I3FrameObject>(*this));
+  else
+    ar & make_nvp("I3CutValuesBase", base_object<I3CutValuesBase>(*this));
   ar & make_nvp("Nchan",Nchan);
   ar & make_nvp("Nhit",Nhit);
   ar & make_nvp("N_1hit",N_1hit);
@@ -34,5 +42,5 @@ void I3CascadeCutValues::serialize(Archive& ar, unsigned version)
   ar & make_nvp("cog",cog);
 }
   
-BOOST_CLASS_VERSION(I3CascadeCutValues, 0);
+BOOST_CLASS_VERSION(I3CascadeCutValues, current_i3cascadecutvalues_version);
 I3_SERIALIZABLE(I3CascadeCutValues);

private/phys-services/I3CutValues.cxx
@@ -19,10 +19,18 @@ void I3CutValues::Calculate(const I3Particle& track,
 
 I3CutValues::~I3CutValues() { }
 
+static const unsigned int current_i3cutvalues_version=2;
+
 template <class Archive>
 void I3CutValues::serialize(Archive& ar, unsigned version)
 {
-  ar & make_nvp("I3FrameObject", base_object<I3FrameObject>(*this));
+  if (version>current_i3cutvalues_version)
+    log_fatal("Attempting to read version %u from file but running version %u of I3CutValues class.",
+              version,current_i3cutvalues_version);
+  if (version<2)
+    ar & make_nvp("I3FrameObject", base_object<I3FrameObject>(*this));
+  else
+    ar & make_nvp("I3CutValuesBase", base_object<I3CutValuesBase>(*this));
   ar & make_nvp("Nchan",Nchan);
   ar & make_nvp("Nhit",Nhit);
   if (version > 0) 
@@ -35,5 +43,14 @@ void I3CutValues::serialize(Archive& ar, unsigned version)
   ar & make_nvp("cog",cog);
 }
   
-BOOST_CLASS_VERSION(I3CutValues, 1);
+BOOST_CLASS_VERSION(I3CutValues, current_i3cutvalues_version);
 I3_SERIALIZABLE(I3CutValues);
+
+template <class Archive>
+void I3CutValuesBase::serialize(Archive& ar, unsigned version)
+{
+	ar & make_nvp("I3FrameObject", base_object<I3FrameObject>(*this));
+}
+
+BOOST_CLASS_VERSION(I3CutValuesBase, 0);
+I3_SERIALIZABLE(I3CutValuesBase);

private/phys-services/I3CutsModule.cxx
@@ -4,9 +4,10 @@
  */
 
 #include "icetray/I3TrayHeaders.h"
+#include "icetray/I3Deprecation.h"
 #include "dataclasses/physics/I3RecoPulse.h"
 #include "dataclasses/geometry/I3Geometry.h"
-#include "phys-services/I3CutsModule.h"
+#include "I3CutsModule.h"
 #include "phys-services/I3CutValues.h"
 #include "phys-services/I3CascadeCutValues.h"
 
@@ -21,7 +22,7 @@ using boost::algorithm::is_any_of;
 using namespace std;
 using namespace I3Units;
 
-I3_MODULE(I3CutsModule);
+I3_DEPRECATED_MODULE(I3CutsModule);
 
 //--------------------------------------------------------------
 I3CutsModule::I3CutsModule(const I3Context& ctx) : I3ConditionalModule(ctx)
@@ -151,7 +152,7 @@ void I3CutsModule::Physics(I3FramePtr frame)
       }
     }
 
-    if(!cuts){
+    if(cuts){
       cuts->Calculate(*particle,geometry,*pulsemap,timeRange_[0],timeRange_[1]);
 
       frame->Put(name+"Cuts"+nameTag_, cuts);

private/phys-services/I3CutsModule.h
@@ -1,5 +1,5 @@
 /**
- * $Id$
+ * $Id: I3CutsModule.h 98103 2013-01-29 17:46:23Z nwhitehorn $
  * (c) 2005 IceCube Collaboration
  * @author D.Turcan
  */

private/phys-services/I3GCDAuditor.cxx
@@ -25,7 +25,7 @@ class I3GCDAuditor : public I3Module
 		    const I3DOMCalibration &cal, const I3DOMStatus &status);
 		std::string bad_dom_list_;
 		bool AMANDA_is_error_;
-		bool fail_on_sim_quantities_;
+		bool max_paranoia_;
 };
 
 I3_MODULE(I3GCDAuditor);
@@ -37,7 +37,8 @@ I3GCDAuditor::I3GCDAuditor(const I3Context& context) : I3Module(context)
 	    true);
 	AddParameter("MaximumParanoia", "Fail for nonsense quantities that "
 	    "are mostly only relevant for simulation (e.g. PMT discriminator "
-	    "thresholds)", true);
+	    "thresholds) as well as things that are unreasonable but not "
+	    "universally fatal", true);
 	AddOutBox("OutBox");
 }
 
@@ -46,7 +47,7 @@ I3GCDAuditor::Configure()
 {
 	GetParameter("BadDOMList", bad_dom_list_);
 	GetParameter("AMANDAIsAnError", AMANDA_is_error_);
-	GetParameter("MaximumParanoia", fail_on_sim_quantities_);
+	GetParameter("MaximumParanoia", max_paranoia_);
 }
 
 void
@@ -59,6 +60,55 @@ I3GCDAuditor::DetectorStatus(I3FramePtr frame)
 	const I3DetectorStatus &status = frame->Get<I3DetectorStatus>();
 
 	bool err = false;
+	
+	// checks for acausal G/C/D frames
+	if (geo.startTime >= geo.endTime) {
+		log_error("Geometry start time %d,%ju is after end time %d,%ju",
+		    geo.startTime.GetUTCYear(),
+		    (uintmax_t)geo.startTime.GetUTCDaqTime(),
+		    geo.endTime.GetUTCYear(),
+		    (uintmax_t)geo.endTime.GetUTCDaqTime());
+		err = true;
+	}
+	if (calib.startTime >= calib.endTime) {
+		log_error("Calibration start time %d,%ju is after "
+		    "end time %d,%ju", calib.startTime.GetUTCYear(),
+		    (uintmax_t)calib.startTime.GetUTCDaqTime(),
+		    calib.endTime.GetUTCYear(),
+		    (uintmax_t)calib.endTime.GetUTCDaqTime());
+		err = true;
+	}
+	if (status.startTime >= status.endTime) {
+		log_error("Detector status start time %d,%ju is after "
+		    "end time: %d,%ju", status.startTime.GetUTCYear(),
+		    (uintmax_t)status.startTime.GetUTCDaqTime(),
+		    status.endTime.GetUTCYear(),
+		    (uintmax_t)status.endTime.GetUTCDaqTime());
+		err = true;
+	}
+
+	#define paranoia(...) if (max_paranoia_) { \
+			log_error(__VA_ARGS__); err = true; \
+		} else { \
+			log_warn(__VA_ARGS__); err = true; \
+		}
+	
+	// Check that various times are reasonable
+	if (status.endTime.GetUTCYear() == 2038 /* 32-bit UNIX end of days */)
+		paranoia("Detector status validity ends in 2038, the UNIX "
+		    "end of days, which is usually wrong");
+
+	if ((status.endTime - status.startTime)/I3Units::gregorianyear >= 1) {
+		paranoia("difference between D end time %d,%ju and "
+		    "start time: %d,%ju is (%f yrs) greater than 1yr, which "
+		    "is likely wrong", status.endTime.GetUTCYear(),
+		    (uintmax_t)status.endTime.GetUTCDaqTime(),
+		    status.startTime.GetUTCYear(),
+		    (uintmax_t)status.startTime.GetUTCDaqTime(),
+		    (status.endTime - status.startTime)/I3Units::gregorianyear);
+	}
+
+	#undef paranoia
 
 	for (I3OMGeoMap::const_iterator i = geo.omgeo.begin();
 	   i != geo.omgeo.end(); i++) {
@@ -102,7 +152,7 @@ bool I3GCDAuditor::CheckDOM(OMKey om, const I3OMGeo &omgeo,
 {
 	#define bad_dom(...) { log_error(__VA_ARGS__); return false; }
 	#define sim_bad_dom(...) { log_error(__VA_ARGS__); \
-	    if (fail_on_sim_quantities_) return false; \
+	    if (max_paranoia_) return false; \
 	    else \
 	    log_error("WARNING! The above error indicates a serious problem, " \
 	     "which has been ignored due to the setting of MaximumParanoia. " \

public/phys-services/I3CutValuesBase.h
@@ -11,6 +11,7 @@
 
 #include <icetray/I3FrameObject.h>
 #include <dataclasses/I3Constants.h>
+#include <dataclasses/Utility.h>
 
 class I3FrameObject;
 template <typename Key, typename Value> struct I3Map;
@@ -27,6 +28,10 @@ class I3CutValuesBase : public I3FrameObject {
                          const I3Map< OMKey, std::vector< I3RecoPulse> >& pulsemap,
                          const double& begTWindow = I3Constants::dt_window_l,
                          const double& endTWindow = I3Constants::dt_window_h) = 0;
+
+ private:
+  friend class boost::serialization::access;
+  template <class Archive> void serialize(Archive & ar, unsigned version);
 };
 
 I3_POINTER_TYPEDEFS(I3CutValuesBase);