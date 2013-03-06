76c549fa63b172b05b84f04b6b5739465126ea59
Jakob van Santen <jvansanten@gmail.com>
2013-03-06 15:57:46 +0000

    Test the serialization of I3SPRNGRandomServiceState, just to be safe. Also, clean up test files when done.
    git-svn-id: http://code.icecube.wisc.edu/svn/projects/phys-services/trunk@100354 16731396-06f5-0310-8873-f7f720988828

--- private/test/I3RandomServiceStateTest.cxx
+++ private/test/I3RandomServiceStateTest.cxx
@@ -2,11 +2,15 @@
 #include <I3Test.h>
 
 #include <phys-services/I3SPRNGRandomService.h>
+#include <icetray/I3Frame.h>
+#include <dataclasses/I3Double.h>
 #include <boost/foreach.hpp>
+#include <fstream>
+#include <stdio.h>
 
 TEST_GROUP(RandomServiceState);
 
-TEST(I3SPRNGRandomService)
+TEST(SPRNGInMemory)
 {
 	I3SPRNGRandomServicePtr rng(new I3SPRNGRandomService(1,1,0, "", "sprng_outstate"));
 	
@@ -48,4 +52,45 @@ TEST(I3SPRNGRandomService)
 		rng->RestoreState(sample.second);
 		ENSURE_EQUAL(sample.first, rng->Exp(M_PI), "Restored RNG returns the same sample");
 	}
+	
+	std::remove("sprng_outstate");
+}
+
+TEST(SPRNGToFile)
+{
+	I3SPRNGRandomServicePtr rng(new I3SPRNGRandomService(1,1,0));
+	
+	typedef std::pair<double, I3FrameObjectPtr> sample_t;
+	std::vector<sample_t> states;
+	
+	I3FramePtr frame(new I3Frame);
+	
+	{
+		std::ofstream outfile("sprng.i3");
+		for (int i = 0; i < 100; i++) {
+			frame->clear();
+			frame->Put("RNGState", rng->GetState());
+			frame->Put("Variate", I3DoublePtr(new I3Double(rng->Exp(M_PI))));
+			frame->save(outfile);
+		}
+	}
+	
+	{
+		std::ifstream infile("sprng.i3");
+		double chance = 0.1;
+		for (int i = 0; i < 100; i++) {
+			frame->clear();
+			frame->load(infile);
+			// Discard random intermediate points, mixing up
+			// the RNG state in the process
+			if (rng->Uniform() > chance)
+				continue;
+			rng->RestoreState(frame->Get<I3FrameObjectConstPtr>("RNGState"));
+			I3DoubleConstPtr v = frame->Get<I3DoubleConstPtr>("Variate");
+			ENSURE_EQUAL(v->value, rng->Exp(M_PI), "Restored RNG returns the same sample");
+		}
+		
+	}
+	
+	std::remove("sprng.i3");
 }
